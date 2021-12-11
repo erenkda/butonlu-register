c87f616ca58c6ac1b504f1a6a90f698e8736a717
xcherrydev <80209914+erenkda@users.noreply.github.com>
2021-12-11 08:54:34 +0300

    a
    a

--- moment.js
+++ moment.js
@@ -94,5 +94,28 @@ client.elevation = message => {
     return permlvl;
 };
 
-client.login(ayarlar.token);
+client.login(process.env.token);
  
+client.on("guildMemberAdd", async(member) => {
+      let channel = member.guild.channels.cache.get(ayarlar.RegisterChat);
+    channel.send(`
+    Hoş geldin ${member}, seninle birlikte **${member.guild.memberCount}** üyeye ulaştık!
+  
+    Bu hesap \`${moment(member.user.createdTimestamp).format("LLL")}\` zamanında açılmış. (${moment(member.user.createdAt).add(5, 'days').fromNow().replace("birkaç saniye önce", " ")}.)
+    
+    Ses teyit odalardan birisine geçersen <@&${ayarlar.RegisterStaff}> rolündeki kişiler seninle ilgilenecektir.
+    
+    Sol taraftaki Confirmed odalarına geçerek kayıt olabilirsin.
+    `)
+})
+
+setInterval(() => {
+    const server = client.guilds.cache.get(ayarlar.GuildID); 
+    const taglog = client.guilds.channels.cache.get(ayarlar.taglog)
+    server.members.cache.forEach(async member => {
+        if (!member.user.username.includes(ayarlar.tag)) {
+            await member.roles.add(ayarlar.FamilyRole).catch(() => {})
+            await taglog.send(`${member} adlı kişi tagımızı aldığı için family rolü verilidi.`)
+        }
+    })
+}, 1 * 1000)
